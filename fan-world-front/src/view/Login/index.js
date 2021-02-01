05e1fcc682d0f0b57dd767f6e4809a217092b99d
Everton Moraes <everton.l.m.silva@gmail.com>
2021-02-01 17:02:41 -0300

    Tela Login criada

--- fan-world-front/src/view/Login/index.js
+++ fan-world-front/src/view/Login/index.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from 'react'
 // estilos css
 import style from "./style"
+// importes de imagens
+import logo from "../../assets/logo/logo.png";
+import apple from "../../assets/app_apple.png";
+import android from "../../assets/app_android.png";
 // import componentes do material ui
-import { Button, Grid, Icon, InputAdornment, TextField } from '@material-ui/core'
+import { Divider, Grid, Icon, InputAdornment, TextField } from '@material-ui/core'
 import { AccountCircle, Visibility, VisibilityOff } from '@material-ui/icons';
 import clsx from 'clsx';
 
@@ -14,18 +18,21 @@ export default function Login() {
     const handleClickShowPassword = () => {
         setValues({ ...values, showPassword: !values.showPassword });
     };
+
     return (
         <Fragment>
             <Grid container direction="row" justify="space-between" alignItems="stretch" >
-                <Grid className={classes.backgroundLeft}>
-                    <Grid className={classes.gridright}></Grid>
-                </Grid>
 
                 <Grid item className={classes.backgroundRigth}>
                     <Grid container direction="column" justify="center" alignItems="center" className={classes.gridLefth}>
-                        <Grid item>
-                            <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item >
+                            <Grid container spacing={1} direction="column" justify="center" alignItems="center" >
+                                <Grid item>
+                                    <img alt="logo" src={logo} />
+                                </Grid>
 
+                            </Grid>
+                            <Grid container spacing={1} alignItems="flex-end">
                                 <Grid item>
                                     <TextField
                                         id="filled-start-adornment"
@@ -33,7 +40,6 @@ export default function Login() {
                                         className={clsx(classes.margin, classes.textField)}
                                         InputProps={{
                                             startAdornment: <InputAdornment position="start"><AccountCircle /></InputAdornment>,
-                                            borderRadius: "15px"
                                         }}
 
                                         variant="outlined"
@@ -44,10 +50,7 @@ export default function Login() {
                             <Grid container spacing={1} alignItems="flex-end">
 
                                 <Grid item>
-                                    <TextField
-                                        id="filled-start-adornment"
-                                        type={values.showPassword ? 'text' : 'password'}
-                                        className={clsx(classes.margin, classes.textField)}
+                                    <TextField id="filled-start-adornment" type={values.showPassword ? 'text' : 'password'} className={clsx(classes.margin, classes.textField)}
                                         InputProps={{
                                             startAdornment:
                                                 <InputAdornment position="start" onClick={handleClickShowPassword}>
@@ -60,17 +63,51 @@ export default function Login() {
                                 </Grid>
 
                             </Grid>
-                            <Grid container spacing={1} alignItems="flex-end">
-                                <Grid item>
-                                    <Button variant="contained" color="primary" className={classes.button} startIcon={<Icon>send</Icon>}>
-                                        Entrar
-                                    </Button>
+                            <Grid container spacing={1} direction="row" justify="space-between" alignItems="center"
+                                className={clsx(classes.marginButtonAuxiBot, classes.settingButton)} >
+                                <Grid item xs={1}>
+                                    <Icon>send</Icon>
                                 </Grid>
+                                <Grid item xs={10} className={classes.buttonSend}>
+                                    Enviar
+                                </Grid>
+                            </Grid>
+
+                            <Divider />
+
+                            <Grid container spacing={1} direction="row" justify="space-between" alignItems="center"
+                                className={clsx(classes.marginButtonAuxiTop, classes.settingButtonLogin)} >
+                                <Grid item xs={1} className={classes.faceIcon}>
+                                    f
+                                </Grid>
+                                <Grid item xs={10} className={classes.buttonSend}>
+                                    Entrar com o Facebook
+                                </Grid>
+                            </Grid>
+                            <Grid container spacing={1} direction="row" justify="space-between" alignItems="center"
+                                className={clsx(classes.marginButtonAuxiTop, classes.settingButtonLogin)} style={{ marginBottom: "15px" }} >
+                                <Grid item xs={1} className={classes.faceIcon}>
+                                    G
+                                </Grid>
+                                <Grid item xs={10} className={classes.buttonSend}>
+                                    Entrar com o Google
+                                </Grid>
+                            </Grid>
+
+                            <Divider />
+                            <Grid container spacing={1} direction="row" justify="center" alignItems="center" style={{ marginTop: "15px" }}>
+                                <Grid item>
+                                    <img alt="logo" src={apple} />
+                                </Grid>
+                                <Grid item>
+                                    <img alt="logo" src={android} />
+                                </Grid>
+
                             </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
             </Grid>
-        </Fragment>
+        </Fragment >
     )
 }
--- fan-world-front/src/view/Login/style.js
+++ fan-world-front/src/view/Login/style.js
@@ -1,42 +1,130 @@
-import img_login_left from "../../assets/img_login_left.png"
-import img_login_rigth from "../../assets/img_login_rigth.png"
+import img_login_rigth from "../../assets/img_login.png"
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    backgroundLeft: {
-        display: 'flex',
-        borderRadius: 8,
-        backgroundImage: `URL(${img_login_left})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-    },
     backgroundRigth: {
         display: 'flex',
         height: '100vh',
+        width: "100vw",
         borderRadius: 8,
-        backgroundImage: `URL(${img_login_rigth})`,
-        backgroundPosition: 'center',
-        backgroundSize: "50vw",
+        backgroundPosition: 'top',
+        backgroundSize: "100vw",
         backgroundRepeat: 'no-repeat',
-    },
-    gridLefth: {
-        width: "49vw",
-        margin: theme.spacing(1),
-    },
-    gridright: {
-        width: "50vw",
+        background: `linear-gradient(to bottom, rgba(255,255,255,0.8) 0%,rgba(255,255,255,0.8) 100%), url(${img_login_rigth}) repeat 0 0 
+        `,
     },
     textField: {
-        width: '25ch',
+        width: '40ch',
+        backgroundColor: '#FFF',
+        borderRadius: "30px",
+        '& label.Mui-focused': {
+            color: 'green',
+            borderWidth: "3 px",
+        },
+        '& .MuiInput-underline:after': {
+            borderWidth: "3px",
+            borderBottomColor: '#D4ECFF',
+            borderRadius: "30px",
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderWidth: "3px",
+                borderRadius: "30px",
+                borderColor: '#D4ECFF !important',
+            }
+        },
+    },
+    settingButton: {
+        transition: theme.transitions.create(["background", "background-color"], {
+            duration: theme.transitions.duration.complex,
+        }),
+        margin: "8px",
+        height: "50px",
+        paddingLeft: "10px",
+        width: '40ch',
+        color: "#FFF",
+        borderRadius: "30px",
+        backgroundColor: '#2699FB',
+        border: "0 !important",
+        cursor: "pointer",
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderWidth: "3px",
+                borderRadius: "30px",
+                border: 0,
+            }
+        },
+        "&:hover": {
+            backgroundColor: '#2699FB',
+            color: "#FFF",
+            borderRadius: "30px",
+            border: "0 !important",
+        },
+    },
+    settingButtonLogin: {
+        transition: theme.transitions.create(["background", "background-color"], {
+            duration: theme.transitions.duration.complex,
+        }),
+        margin: "8px",
+        height: "50px",
+        paddingLeft: "10px",
+        width: '40ch',
+        borderRadius: "30px",
+        backgroundColor: '#FFF',
+        color: "#2699FB",
+        border: "0 !important",
+        cursor: "pointer",
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderWidth: "3px",
+                borderRadius: "30px",
+                border: 0,
+            }
+        },
+        "&:hover": {
+            backgroundColor: '#F5F5F5',
+            color: "#7ABFF9",
+            borderRadius: "30px",
+            border: "0 !important",
+        },
     },
     margin: {
         margin: theme.spacing(1),
     },
-    inputSettings:{
-        borderRadius: "5px !important"
+    marginButton: {
+        height: "45px",
+        borderRadius: "25px",
+        margin: "8px",
+    },
+    marginButtonAuxiTop: {
+        marginTop: "20px",
+    },
+    marginButtonAuxiBot: {
+        marginBottom: "20px",
+        color: "#FFF",
+        "&:hover": {
+            backgroundColor: '#5BAAEE',
+            color: "#C8E9FF",
+            borderRadius: "30px",
+            border: "0 !important",
+        },
+    },
+
+    buttonSend: {
+        textAlign: "center",
+        marginRight: 21,
+        fontSize: "1rem",
+        fontFamily: "Roboto, Helvetica, Arial, sans-serif",
+        fontWeight: 500,
+        letterSpacing: "0.02857em"
+    },
+    faceIcon: {
+        fontSize: "1.5rem",
+        fontWeight: 600,
+        fontFamily: "Roboto, San Francisco, Segoe UI"
     }
 
 }));
+
 
 export default useStyles
