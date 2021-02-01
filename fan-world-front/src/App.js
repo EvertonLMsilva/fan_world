e443de0471e46a1dd083a3943fd1d521a978167c
Everton Moraes <everton.l.m.silva@gmail.com>
2021-02-01 17:09:36 -0300

    Criando tela dahsboard

--- fan-world-front/src/App.js
+++ fan-world-front/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import TopBar from "./components/TopBar";
 import "./style.css"
 import Login from "./view/Login";
+import Dashboard from "./view/Login/Dashboard";
 
 function App() {
   return (
     <div className="root">
-      <Login />
+      <Dashboard />
     </div>
 
   )
